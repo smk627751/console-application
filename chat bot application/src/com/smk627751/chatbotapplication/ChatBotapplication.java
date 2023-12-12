9bf41446191be979da36b17afd2a2f06952f89ff
smk627751 <smk627751@gmail.com>
2023-12-12 12:38:25 +0530

    updated commit

--- chat bot application/src/com/smk627751/chatbotapplication/ChatBotapplication.java
+++ chat bot application/src/com/smk627751/chatbotapplication/ChatBotapplication.java
@@ -1,10 +1,6 @@
 package com.smk627751.chatbotapplication;
 
-import java.util.Map;
-import java.util.Scanner;
-
 import com.smk627751.chatbotapplication.view.View;
-import com.smk627751.chatbotapplication.viewmodel.ViewModel;
 
 public class ChatBotapplication {
 	private View view;
